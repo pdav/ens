accaafd59837936437725707cf7334967c607a6a
Pierre DAVID <pdagog@gmail.com>
2017-03-23 11:47:50 +0100

    Ajout de l'attribut "unused" pour compilation avec gcc

---
+++ cours-se/inc1-7-sig/proto.c
@@ -0,0 +1,5 @@
+/* argument : numero du signal recu */
+void fct (int signum)
+{
+    /* code de la fonction */
+}
---
+++ cours-se/inc1-7-sig/unused.c
@@ -0,0 +1,5 @@
+/* attribut specifique gcc (et clang) */
+void fct (__attribute__ ((unused)) int signum)
+{
+    /* code de la fonction */
+}
